deb4f32bc0285100f553ba4d6fb2e1770a3121c9
Kuboss949 <jakub.lopata@student.pk.edu.pl>
2023-06-21 21:52:55 +0200

    Zrobione

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
     public static HashMap<Integer, Integer> sortByValues(HashMap<Integer,Integer> map){
         List<HashMap.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
+        Collections.sort(list, (o1, o2) -> o1.getValue().compareTo(o2.getValue()));
         HashMap<Integer, Integer> sortedMap = new LinkedHashMap<>();
         for (Map.Entry<Integer, Integer> entry : list) {
             sortedMap.put(entry.getKey(), entry.getValue());
